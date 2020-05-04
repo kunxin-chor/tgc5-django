cf6aaf664f61e2d1f7b00f156aef483112fc6898
kunxin-chor <chorkunxin@yahoo.com>
2020-05-04 03:11:49 +0000

    Added templates

--- todo/views.py
+++ todo/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, 'todo/index.template.html')
