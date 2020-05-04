ada011e2f8c3dcde9c8435e605906466de9801e5
kunxin-chor <chorkunxin@yahoo.com>
2020-05-04 07:31:18 +0000

    Added Read Todos

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+# This class represents the Todo table in the database
+# Todo will inherit from models.Model; the latter is a Django class
+# that already has the funcitonality we need. By inheriting, Todo will
+# have all those functionalities
+class Todo(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    done = models.BooleanField(blank=False, default=False)
+
+    def __str__(self):
+        return self.name
+
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from .models import Todo
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'todo/index.template.html')
+    all_todos = Todo.objects.all()
+    return render(request, 'todo/index.template.html', {
+        'all_todos':all_todos
+    })
